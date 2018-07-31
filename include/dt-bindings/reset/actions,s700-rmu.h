a3ffb3ff91cf2005d6dbc97e2eccf0d2450c685e
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2018-07-31 22:55:30 +0530

    dt-bindings: reset: Add Actions Semi S700 SoC RMU support
    Add RMU (Reset Management Unit) support for the Actions Semi S700
    SoC which is a part of the Actions Semi Owl family series.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

---
+++ include/dt-bindings/reset/actions,s700-rmu.h
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: (GPL-2.0-or-later OR MIT)
+//
+// Device Tree binding constants for Actions Semi S700 Reset Management Unit
+//
+// Copyright (c) 2018 Linaro Ltd.
+
+#ifndef _DT_BINDINGS_ACTIONS_S700_RESET_H
+#define _DT_BINDINGS_ACTIONS_S700_RESET_H
+
+#define S700_RESET_AUDIO		0
+#define S700_RESET_CSI			1
+#define S700_RESET_DE			2
+#define S700_RESET_DSI			3
+#define S700_RESET_GPIO			4
+#define S700_RESET_I2C0			5
+#define S700_RESET_I2C1			6
+#define S700_RESET_I2C2			7
+#define S700_RESET_I2C3			8
+#define S700_RESET_KEY			9
+#define S700_RESET_LCD0			10
+#define S700_RESET_SI			11
+#define S700_RESET_SPI0			12
+#define S700_RESET_SPI1			13
+#define S700_RESET_SPI2			14
+#define S700_RESET_SPI3			15
+#define S700_RESET_UART0		16
+#define S700_RESET_UART1		17
+#define S700_RESET_UART2		18
+#define S700_RESET_UART3		19
+#define S700_RESET_UART4		20
+#define S700_RESET_UART5		21
+#define S700_RESET_UART6		22
+
+#endif /* _DT_BINDINGS_ACTIONS_S700_RESET_H */
